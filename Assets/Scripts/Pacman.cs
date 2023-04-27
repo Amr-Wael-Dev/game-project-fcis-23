fd766a6bf82f67b5f2406941282c7d81c61ac576
Amr-Wael-Dev <amr.wael.dev@gmail.com>
2023-04-27 15:49:03 +0200

    Add pacman movement and rotation

--- Assets/Scripts/Pacman.cs
+++ Assets/Scripts/Pacman.cs
@@ -1,18 +1,27 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Movement))]
 public class Pacman : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    public Movement movement { get; private set; }
+
+    private void Awake() {
+        movement = GetComponent<Movement>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyUp(KeyCode.UpArrow)) {
+            movement.SetDirection(Vector2.up);
+        } else if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyUp(KeyCode.LeftArrow)) {
+            movement.SetDirection(Vector2.left);
+        } else if (Input.GetKeyDown(KeyCode.S) || Input.GetKeyUp(KeyCode.DownArrow)) {
+            movement.SetDirection(Vector2.down);
+        } else if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyUp(KeyCode.RightArrow)) {
+            movement.SetDirection(Vector2.right);
+        }
+
+        float rotationAngle = Mathf.Atan2(movement.direction.y, movement.direction.x);
+        transform.rotation = Quaternion.AngleAxis(rotationAngle * Mathf.Rad2Deg, Vector3.forward);
     }
 }
